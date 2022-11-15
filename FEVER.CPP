09a090d9182dc1e37f0294f69c7a390e9d28bea8
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2022-11-15 09:56:23 +0530

    Create FEVER.CPP

---
+++ FEVER.CPP
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int s;
+	    cin>>s;
+	    if(s>98) cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+	}
+	return 0;
+}
