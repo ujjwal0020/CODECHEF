4747eb88d77bb07139810c3eb2c87fef86ea453b
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2022-11-16 09:33:50 +0530

    Create CHEF AND CHAPTERS.CPP

---
+++ CHEF AND CHAPTERS.CPP
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+for(int i=0;i<t;i++)
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    int s;
+    s=a*b;
+    int d;
+    d=s*c;
+    cout<<d<<endl;
+}
+	return 0;
+}
