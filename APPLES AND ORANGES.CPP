3aad9c14a0a284090c58fb9e199f4a4cdd77b502
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2022-11-16 09:44:59 +0530

    Create APPLES AND ORANGES.CPP

---
+++ APPLES AND ORANGES.CPP
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	int a,b,c;
+	cin>>a>>b;
+	c=a+b;
+	if(x>=c) cout<<"yes"<<endl;
+	else cout<<"no"<<endl;
+	return 0;
+}
