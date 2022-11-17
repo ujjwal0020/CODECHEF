ee636e7f312d2751d55ae6fe9611dff513ded50f
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2022-11-17 16:21:39 +0530

    Create IS IT HOT OR COLD.CPP

---
+++ IS IT HOT OR COLD.CPP
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int c;
+	    cin>>c;
+	    if(c>20) cout<<"hot"<<endl;
+	    else cout<<"cold"<<endl;
+	}
+	return 0;
+}
