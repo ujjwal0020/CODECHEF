197bf1fbf09e545f1b358433dd4442b1e5140baf
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2022-11-19 15:46:19 +0530

    Create CHEF AND WIRE FRAMES.CPP

---
+++ CHEF AND WIRE FRAMES.CPP
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,a,b,c,d,e;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    cin>>a>>b>>c;
+	    d=a*2+b*2;
+	    e=c*d;
+	    cout<<e<<endl;
+	}
+	return 0;
+}
