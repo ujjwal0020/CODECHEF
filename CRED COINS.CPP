ae47ebeb3017c0934426780a6f24b96b9ceae4a2
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2022-11-27 21:42:37 +0530

    Create CRED COINS.CPP

---
+++ CRED COINS.CPP
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    int c;
+	    c=a*b;
+	    int d;
+	    d=c/100;
+	    cout<<d<<endl;
+	}
+	return 0;
+}
