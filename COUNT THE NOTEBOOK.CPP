76cfc0550a49914faaad8a90b5b93f574334a688
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2022-11-28 23:27:20 +0530

    Create COUNT THE NOTEBOOK.CPP

---
+++ COUNT THE NOTEBOOK.CPP
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int t;
+cin>>t;
+for(int i=0;i<t;i++)
+{
+    int a;
+    cin>>a;
+    int d;
+    d=(a*1000)/100;
+    cout<<d<<endl;
+}
+	return 0;
+}
