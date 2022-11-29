727278d681d799576070ef36d1316a19d40ef965
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2022-11-29 09:52:40 +0530

    Create JANMANSH AND ASSIGNEMENT.CPP

---
+++ JANMANSH AND ASSIGNEMENT.CPP
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int a;
+	    cin>>a;
+	    if(10-a>=3) cout<<"yes"<<endl;
+	    else cout<<"no"<<endl;
+	}
+	return 0;
+}
