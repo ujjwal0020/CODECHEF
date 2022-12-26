3718abcd074b36a0d5b5fe8164a0a0cc2ed8a1d1
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2022-12-26 23:21:08 +0530

    Create PASSES FOR FAIR.CPP

---
+++ PASSES FOR FAIR.CPP
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	int n,k;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    cin>>n>>k;
+	    if(k>n)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
