47024b73487b8bb8183f6a8c657981f8226b785c
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2023-01-09 10:11:06 +0530

    Create EXPENSE LIST.CPP

---
+++ EXPENSE LIST.CPP
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{
+	    int n,x;
+	    cin>>n>>x;
+	    int a=pow(2,x-n);
+	    cout<<a<<endl;
+	}
+	return 0;
+}
