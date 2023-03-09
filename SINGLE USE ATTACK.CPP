effd73f9e88850bb4aaabb43bb41f8ca9f548822
UJJWAL SAHU <113961256+ujjwal0020@users.noreply.github.com>
2023-03-09 23:28:03 +0530

    Create SINGLE USE ATTACK.CPP

---
+++ SINGLE USE ATTACK.CPP
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int H,X,Y;
+        cin>>H>>X>>Y;
+        int special_power = H-Y;
+        int normal_power = special_power%X;
+        if(normal_power == 0){
+            cout<< 1 + special_power/X << endl;
+        }
+        else{
+            cout<< 2 + special_power/X << endl;
+        }
+    }return 0;
+}
